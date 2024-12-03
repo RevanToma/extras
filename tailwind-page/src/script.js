7f7c9048e3e3765846aee8954958326e85ab6932
revan <tamrevan@gmail.com>
2024-12-03 10:54:45 +0100

    added pritteier

--- tailwind-page/src/script.js
+++ tailwind-page/src/script.js
@@ -1,25 +1,19 @@
 const button = document.getElementById("menu-button"),
   menuContent = document.getElementById("menu-content"),
   menuCloseBtn = document.getElementById("menu-close"),
-  overLay = document.getElementById("overlay"),
-  searchBtn = document.getElementById("searchBtn"),
-  docSearchModal = document.getElementById("docSearchModal")
+  overLay = document.getElementById("overlay");
 
 button.addEventListener("click", () => {
-    menuContent.classList.toggle("hidden");
- overLay.classList.toggle("hidden")
-})
+  menuContent.classList.toggle("hidden");
+  overLay.classList.toggle("hidden");
+});
 
 menuCloseBtn.addEventListener("click", () => {
-    menuContent.classList.toggle("hidden");
-    overLay.classList.toggle("hidden")
-})  
+  menuContent.classList.toggle("hidden");
+  overLay.classList.toggle("hidden");
+});
 
 overLay.addEventListener("click", () => {
-    menuContent.classList.toggle("hidden");
-    overLay.classList.toggle("hidden")
-})
-
-searchBtn.addEventListener("click", () => {
-    docSearchModal.classList.toggle("hidden")
-})
+  menuContent.classList.toggle("hidden");
+  overLay.classList.toggle("hidden");
+});
