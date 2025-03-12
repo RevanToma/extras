ff6ddb0afda14aab2ef6e0072a23bbbf4d66b4c8
RevanToma <tamrevan@gmail.com>
2025-03-12 16:24:31 +0100

    fixed hydration error and connecting to hosted backend

--- banksajt/banksajt-frontend/components/mode-toggle.tsx
+++ banksajt/banksajt-frontend/components/mode-toggle.tsx
@@ -14,11 +14,16 @@ import {
 import { Check } from 'lucide-react';
 
 export function ModeToggle() {
-  const { setTheme, theme } = useTheme();
+  const { setTheme, theme } = useTheme(),
+    [isMounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => setIsMounted(true), []);
+
+  if (!isMounted) return null;
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
+      <DropdownMenuTrigger asChild>
         <Button variant='outline' className='cursor-pointer'>
           <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 ' />
           <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 ' />
